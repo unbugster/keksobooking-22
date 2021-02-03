ca9df2b57afb718913154019ef02c6c5627d1d16
Andrey Bagaev <unbugster@gmail.com>
2021-02-03 17:55:52 +0300

    Добавляет main.js в html, добавляет функции возвращающие числа из заданных диапазонов

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const getRandomIntInclusive = (min, max) => {
+  try {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    if (isNaN(min) || isNaN(max) || min >= max || min < 0)
+      throw new Error('Введены некорректные данные');
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } catch (err) {
+    alert('Все плохо');
+  }
+};
+
+const getRandomInt = (min, max, decimalPlaces = 0) => {
+  try {
+    if (isNaN(min) || isNaN(max) || min >= max || min < 0)
+      throw new Error('Введены некорректные данные');
+    return Number(
+      (Math.random() * (max - min) + min).toFixed(decimalPlaces));
+  } catch (err) {
+    alert('Все очень плохо!');
+  }
+};
