68261abb29baed6c62690794e22463cc6e4cfce6
Andrey Bagaev <unbugster@gmail.com>
2021-02-27 17:06:02 +0300

    Вносит исправления после проверки наставником

--- js/main.js
+++ js/main.js
@@ -1,49 +1,7 @@
-// Returns random integer from the passed range inclusive
-const getRandomBetween = (min, max) => {
-  if (isNaN(min) || isNaN(max)) {
-    throw new Error('Invalid argument type entered.');
-  }
-  if (min < 0 || max < 0) {
-    throw new Error('A negative value has been entered.');
-  }
-  if (min === max) {
-    return min;
-  }
-  if (min > max) {
-    [min, max] = [max, min];
-  }
-
-  return (
-    Math.floor(Math.random() * (Math.floor(max) - Math.floor(min) + 1)) +
-    Math.floor(min)
-  );
-};
-
-try {
-  getRandomBetween(2, 10);
-} catch (err) {
-  alert('Something went wrong, contact your administrator');
-  /* eslint-disable no-console */
-  console.error(err.name + ':' + err.message);
-}
-
-// Returns a random floating point number from the passed range inclusive.
-const getRandomFloatBetween = (min, max, decimalPlaces = 0) => {
-  if (isNaN(decimalPlaces) || decimalPlaces < 0) {
-    throw new Error('Invalid argument entered.');
-  }
-  const multiplier = Math.pow(10, decimalPlaces);
-
-  return getRandomBetween(min * multiplier, max * multiplier) / multiplier;
-};
-
-try {
-  getRandomFloatBetween(0, 5.12, 3);
-} catch (err) {
-  alert('Something went wrong, contact your administrator');
-  /* eslint-disable no-console */
-  console.error(err.name + ':' + err.message);
-}
+const OFFERS_COUNT = 10;
+const DIGITS_COUNT = 2;
+const TYPES = ['palace', 'flat', 'house', 'bungalow'];
+const CHECKINS_CHECKOUTS = ['12:00', '13:00', '14:00'];
 
 const TITLES = [
   'Апартаменты Villa Ikebukuro.',
@@ -55,10 +13,6 @@ const TITLES = [
   'Дом для отпуска Araiya в самом центре Токио.',
   'Отель для некурящих Asakusa Ryokan Toukaisou.',
 ];
-
-const TYPES = ['palace', 'flat', 'house', 'bungalow'];
-
-const CHECKINS_CHECKOUTS = ['12:00', '13:00', '14:00'];
 
 const FEATURES = [
   'wifi',
@@ -83,20 +37,57 @@ const DESCRIPTIONS = [
   'Все номера отеля оснащены чайником, кондиционером и телевизором.',
   'Расстояние до международного аэропорта Токио-Ханэда составляет 30 км.',
 ];
-const OFFERS_COUNT = 10;
-const MIN_GUEST_COUNT = 1;
-const MAX_GUEST_COUNT = 6;
-const MIN_LOCATION_X = 35.65;
-const MAX_LOCATION_X = 35.7;
-const MIN_LOCATION_Y = 139.7;
-const MAX_LOCATION_Y = 139.8;
-const MIN_PRICE = 5000;
-const MAX_PRICE = 50000;
-const MIN_ROOM_COUNT = 1;
-const MAX_ROOM_COUNT = 4;
-const DIGITS_COUNT = 2;
+const Guests = {
+  min:1,
+  max:6,
+}
+const Coordinates = {
+  minX: 35.65,
+  maxX: 35.7,
+  minY:139.7,
+  maxY:139.8,
+}
 
+const Prices = {
+  min: 5000,
+  max: 50000,
+}
 
+const Rooms = {
+  min:1,
+  max:4,
+}
+
+// Returns random integer from the passed range inclusive
+const getRandomBetween = (...args) => {
+  let [min, max] = args;
+
+  if (isNaN(min) || isNaN(max)) {
+    throw new Error('Invalid argument type entered.');
+  }
+  if (min < 0 || max < 0) {
+    throw new Error('A negative value has been entered.');
+  }
+  if (min === max) {
+    return min;
+  }
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
+  return (
+    Math.floor(Math.random() * (max - min + 1)) + Math.floor(min));
+};
+
+// Returns a random floating point number from the passed range inclusive.
+const getRandomFloatBetween = (min, max, decimalPlaces = 0) => {
+  if (isNaN(decimalPlaces) || decimalPlaces < 0) {
+    throw new Error('Invalid argument entered.');
+  }
+  const multiplier = Math.pow(10, decimalPlaces);
+
+  return getRandomBetween(min * multiplier, max * multiplier) / multiplier;
+};
 
 const getAvatarImg = (i) => {
   const avatarId = `${i}`.padStart(2, '0');
@@ -111,16 +102,20 @@ const getRandomArrayElement = (array) => {
 // Функция возращает случайный массив рандомной длины
 const getRandomArrayList = (array) => {
   const shouldTakeNumber = () => Boolean(getRandomBetween(0, 1));
+  let result = [];
+  while (!result.length) {
+    result = array.filter(shouldTakeNumber)
+  }
 
-  return array.filter(shouldTakeNumber);
+  return result;
 };
 
 // Функция, возвращающая 1 нужный объект
 const createAd = () => {
-  const coordX = getRandomFloatBetween(MIN_LOCATION_X, MAX_LOCATION_X, DIGITS_COUNT);
-  const coordY = getRandomFloatBetween(MIN_LOCATION_Y, MAX_LOCATION_Y, DIGITS_COUNT);
-  const checkin = getRandomArrayElement(CHECKINS_CHECKOUTS);
-  const checkout = getRandomArrayElement(CHECKINS_CHECKOUTS);
+  const coordX = getRandomFloatBetween(Coordinates.minX, Coordinates.maxX, DIGITS_COUNT);
+  const coordY = getRandomFloatBetween(Coordinates.minY, Coordinates.maxY, DIGITS_COUNT);
+  const check = getRandomArrayElement(CHECKINS_CHECKOUTS);
+
   return {
     author: {
       avatar: getAvatarImg(getRandomBetween(0, 8)),
@@ -128,12 +123,12 @@ const createAd = () => {
     offer: {
       titles: getRandomArrayElement(TITLES),
       address: {x: coordX, y: coordY},
-      price: getRandomBetween(MIN_PRICE, MAX_PRICE),
+      price: getRandomBetween(Prices.min, Prices.max),
       type: getRandomArrayElement(TYPES),
-      rooms: getRandomBetween(MIN_ROOM_COUNT , MAX_ROOM_COUNT),
-      guests: getRandomBetween(MIN_GUEST_COUNT, MAX_GUEST_COUNT),
-      checkin:  checkin,
-      checkout: checkout,
+      rooms: getRandomBetween(Rooms.min, Rooms.max),
+      guests: getRandomBetween(Guests.min, Guests.max),
+      checkin:  check,
+      checkout: check,
       features: getRandomArrayList(FEATURES),
       description: getRandomArrayElement(DESCRIPTIONS),
       photos: getRandomArrayList(PHOTOS),
@@ -150,6 +145,7 @@ const createAds = (offersCount) => {
   for (let i = 0; i < offersCount; i++) {
     ads.push(createAd());
   }
+
   return ads;
 };
 /* eslint-disable no-console */
