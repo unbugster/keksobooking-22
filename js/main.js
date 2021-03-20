813b4ccabc95c2f174b943c80fda7c2f17d25526
Andrey Bagaev <unbugster@gmail.com>
2021-03-20 17:16:05 +0300

    Добавляет обработку пользовательского ввода в форме

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import {renderAds} from './similar-ads.js';
 import {createAds, OFFERS_COUNT} from './data.js';
+import {initFormListeners} from './form.js';
 
 const ads = createAds(OFFERS_COUNT);
 renderAds(1, ads)
+initFormListeners();
