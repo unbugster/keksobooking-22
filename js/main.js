34aa7ae7017b578032048f549386b05b818444cb
Andrey Bagaev <unbugster@gmail.com>
2021-03-20 15:26:59 +0300

    Вносит исправления после проверки наставником

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-import './similar-ads.js';
+import {renderAds} from './similar-ads.js';
+import {createAds, OFFERS_COUNT} from './data.js';
+
+const ads = createAds(OFFERS_COUNT);
+renderAds(1, ads)
