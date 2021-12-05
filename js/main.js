b313796bd5d3b379302a5ab21df2631318284b12
Andrey Bagaev <unbugster@gmail.com>
2021-12-05 21:01:44 +0300

    Add syntax corrections

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно.
 const getRandomInt = (min, max) => {
   if (isNaN(min) || isNaN(max)) {
     throw new Error('Invalid argument type entered.');
@@ -16,19 +15,16 @@ const getRandomInt = (min, max) => {
     return min;
   }
 
-  return Math.floor(Math.random() * (max - min + 1)) + Math.floor(min);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-
-// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-
-const getRandomFloatBetween = (min, max, floatingPoint = 0) => {
-  if (isNaN(floatingPoint) || floatingPoint < 0) {
+const getRandomFloatBetween = (min, max, decimalPlaces = 0) => {
+  if (isNaN(decimalPlaces) || decimalPlaces < 0) {
     throw new Error('Invalid argument entered.');
   }
-  const decimalPlaces = Math.pow(10, floatingPoint);
+  const multiplier = Math.pow(10, decimalPlaces);
 
-  return getRandomInt(min * decimalPlaces, max * decimalPlaces) / decimalPlaces;
+  return getRandomInt(min * multiplier, max * multiplier) / multiplier;
 };
 
 const TITLES = [
@@ -42,29 +38,30 @@ const TITLES = [
 const PRICE = {
   min: 2000,
   max: 50000,
-}
+};
 
 const TYPE = [
   'palace',
   'flat',
   'house',
   'bungalow',
-]
+];
+
 const ROOMS = {
   min: 1,
   max: 4,
-}
+};
 
 const GUESTS = {
   min: 1,
   max: 6,
-}
+};
 
 const CHECKS = [
   '12:00',
   '13:00',
   '14:00',
-]
+];
 
 const FEATURES = [
   'wifi',
@@ -73,27 +70,27 @@ const FEATURES = [
   'washer',
   'elevator',
   'conditioner',
-]
+];
 
 const DESCRIPTION = [
   'good view',
   'city center',
   'beside the beach',
   'best sunsets ever',
-]
+];
 
 const PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-]
+];
 
 const LOCATIONS = {
   minX: 35.65000,
   maxX: 35.70000,
   minY: 139.70000,
   maxY: 139.80000,
-}
+};
 
 const DIGITS_COUNT = 2;
 
@@ -103,8 +100,9 @@ const getRandomList = (array) => {
       return el;
     }
   })
+
   return newArray;
-}
+};
 
 const getRandomArrayElement = (array) => {
   return array[getRandomInt(0, array.length - 1)];
@@ -113,7 +111,7 @@ const getRandomArrayElement = (array) => {
 
 let formattedNumber = (num) => {
   return ('0' + num).slice(-2);
-}
+};
 
 const getRandomAd = () => {
   let myNumber = getRandomInt(1, 8);
@@ -124,7 +122,6 @@ const getRandomAd = () => {
     author: {
       avatar: `img/avatars/user${formattedNumber(myNumber)}.png`,
     },
-
     offer: {
       title: getRandomArrayElement(TITLES),
       address: `${locationX} + ${locationY}`,
@@ -138,18 +135,21 @@ const getRandomAd = () => {
       description: DESCRIPTION[getRandomInt(0, 3)],
       photos: getRandomList(PHOTOS),
     },
-
     location: {
       x: locationX,
       y: locationY,
     },
   }
-}
+};
+
 const getTenRandomAds = () => {
   let result = [];
+
   for (let i = 1; i <= 10; i++) {
     result.push(getRandomAd());
   }
+
   return result;
-}
+};
+
 console.log(getTenRandomAds())
