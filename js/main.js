6fa93bca5d42200b7e002834fc8bafc845d23002
unbugster <unbugster@gmail.com>
2022-03-30 13:08:02 +0300

    Fix module interaction

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 import { generateRandomAds, mapCanvas } from './data.js';
-// import { generateAds } from './similar-ads.js';
 import { addFormListeners } from './form.js';
-import { isFormActive } from './inactive-form.js';
 import { addPins } from './map.js';
 
 const ads = generateRandomAds();
 addPins(ads)
 addFormListeners();
-isFormActive(true);
+
