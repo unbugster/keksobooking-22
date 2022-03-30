b7070aff547cdd9297d1d4d7c31cce1f961b582e
unbugster <unbugster@gmail.com>
2022-03-30 10:43:43 +0300

    Main merge comm

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { generateRandomAds, mapCanvas } from './data.js';
 import { generateAds } from './similar-ads.js';
+<<<<<<< HEAD
 import { formListeners } from './form.js';
 import { isFormActive } from './inactive-form.js';
 import { addPins } from './map.js';
@@ -11,3 +12,11 @@ const tenRandomAdsFragment = generateAds(ads);
 console.log('tenRandomAdsFragment', tenRandomAdsFragment);
 formListeners();
 isFormActive(true);
+=======
+import { addFormListeners } from './form.js';
+
+const ads = generateRandomAds();
+const tenRandomAds = generateAds(ads);
+mapCanvas.appendChild(tenRandomAds.children[0]);
+addFormListeners();
+>>>>>>> main
