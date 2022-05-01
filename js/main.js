2acde38b2af9e2b38ef1ca7530d818afbc37b4a1
unbugster <unbugster@gmail.com>
2022-05-01 20:46:33 +0300

    Add limit to the number of pin ads displayed

--- js/main.js
+++ js/main.js
@@ -4,10 +4,12 @@ import { generateAdElement } from './similar-ads.js';
 import { getAdsData } from './data.js';
 import { openSuccessPopup, openDataErrorPopup, showAlert } from './popup.js';
 
+const SIMILAR_ADS = 10;
 initMap(toggleAdFormsActivation);
 
 getAdsData().then((pinsData) => {
-  renderPins(pinsData, generateAdElement);
+  const renderedPins = pinsData.slice(0, SIMILAR_ADS);
+  renderPins(renderedPins, generateAdElement);
 }).catch(() => {
   openDataErrorPopup();
 });
